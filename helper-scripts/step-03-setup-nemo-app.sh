2de95585b7024396790fe011a14fef8aa5d6f35b
Ryan Taylor <ryant@eng.utah.edu>
2018-02-09 16:40:10 -0700

    add "run server" part

--- helper-scripts/step-03-setup-nemo-app.sh
+++ helper-scripts/step-03-setup-nemo-app.sh
@@ -22,3 +22,6 @@ wget -O /home/nemo/python/lib/python3.6/site-packages/NEMO/views/fake_authentica
 
 
 # gunicorn -b 0.0.0.0:8000 NEMO.wsgi:application
+
+# run in dev mode:
+python3 /home/nemo/python/lib/python3.6/site-packages/NEMO/manage.py runserver
