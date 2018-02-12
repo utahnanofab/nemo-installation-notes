f97902dca42afa1941d0d19604af1e9f0768ef44
Ryan Taylor <ryant@eng.utah.edu>
2018-02-12 09:19:48 -0700

    remove ~

--- helper-scripts/step-04-create-icon.sh
+++ helper-scripts/step-04-create-icon.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-mkdir -p ~/.config/autostart ~/Desktop
+mkdir -p /home/nemo/.config/autostart /home/nemo/Desktop
 
 echo '[Desktop Entry]
 Version=1.0
 Type=Application
 Terminal=true
-Exec=python3 ~/python/lib/python3.6/site-packages/NEMO/manage.py runserver
+Exec=python3 /home/nemo/python/lib/python3.6/site-packages/NEMO/manage.py runserver
 Name=Start NEMO Development Server
-Icon=' > ~/Desktop/nemo.desktop
+Icon=' > /home/nemo/Desktop/nemo.desktop
 
 echo '[Desktop Entry]
 Version=1.0
@@ -16,9 +16,9 @@ Type=Application
 Terminal=false
 Exec=firefox http://localhost:8000
 Name=Open Firefox for NEMO
-Icon=' > ~/Desktop/firefox-nemo.desktop
+Icon=' > /home/nemo/Desktop/firefox-nemo.desktop
 
-chmod 755 ~/Desktop/nemo.desktop ~/Desktop/firefox-nemo.desktop
+chmod 755 /home/nemo/Desktop/nemo.desktop /home/nemo/Desktop/firefox-nemo.desktop
 
 
-cp ~/Desktop/nemo.desktop ~/Desktop/firefox-nemo.desktop ~/.config/autostart
+cp /home/nemo/Desktop/nemo.desktop /home/nemo/Desktop/firefox-nemo.desktop /home/nemo/.config/autostart
